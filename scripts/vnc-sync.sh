2ca1cda165a40ca91edb613de554dbe154d0dcb9
Wu Zhangjin <wuzhangjin@gmail.com>
2017-04-04 21:25:12 +0800

    Sync with vplayer and vtools
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- scripts/vnc-sync.sh
+++ scripts/vnc-sync.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-# vnc-prepare.sh -- Add vplayer and vtools in system/web/noVNC, should run before building tinylab/cloud-ubuntu-web
+# vnc-sync.sh -- Add vplayer and vtools in system/web/noVNC, should run before building tinylab/cloud-ubuntu-web
 #
 
 TOP_DIR=$(cd $(dirname $0)/../ && pwd)
 
-# https://github.com/tinyclub/vplayer.io.git 
+# https://github.com/tinyclub/vplayer.io.git
 git submodule update --init --remote ${TOP_DIR}/system/web/noVNC/
 
 # https://github.com/tinyclub/vtools.git
